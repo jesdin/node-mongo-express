2b8b1629cd9eb7c3c0cbe5d308bbf27bebb025c6
Jesdin Raphael <jesdinraphael@gmail.com>
2018-07-06 15:53:07 +0530

    made a page to fetch data from data base

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ your server is hosted on "http://localhost:3000/"
 
 ## to access the hello world page
 go to "http://localhost:3000/helloworld"
+
+## to view the page which is showing data from mongo db
+go to "http://localhost:3000/userlist"
--- routes/index.js
+++ routes/index.js
@@ -12,3 +12,14 @@ module.exports = router;
 router.get('/helloworld', function(req, res) {
   res.render('helloworld', { title: 'Hello, World!' });
 });
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('users');
+  collection.find({},{},function(e,docs){
+      res.render('userlist', {
+          "userlist" : docs
+      });
+  });
+});
